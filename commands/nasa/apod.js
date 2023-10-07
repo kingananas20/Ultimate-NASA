db3313d044d866fd701e79da0194d08859f8e9d3
kingananas20 <jonasarter@outlook.com>
2023-10-07 18:15:56 +0200

    Made changes to subcommand date and begun with subcommand range

--- commands/nasa/apod.js
+++ commands/nasa/apod.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const ColorThief = require("colorthief");
-const moment = require("moment");
 require("dotenv").config();
+
+let url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.APIKEY}`;
 
 function isValidDate(dateString) {
   const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
@@ -55,31 +56,67 @@ module.exports = {
     const subcommand = interaction.options.getSubcommand();
 
     if (subcommand === "date") {
-      let url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.APIKEY}`;
-      try {
-        const date = interaction.options.get("date").value;
+      const date = interaction.options.get("date");
 
-        if (!isDateInRange(date))
-          throw new Error("The provided date is invalid.");
-
-        if (isCurrentDate(date)) url = url + `&date=${date}`;
-      } catch (error) {
-        interaction.reply({ content: error.message, ephemeral: true });
-        return;
+      if (date) {
+        if (isDateInRange(date["value"]) && !isCurrentDate(date["value"]))
+          url += `&date=${date["value"]}`;
       }
+
+      url += "&thumbs=true";
 
       async function fetchData(url) {
         try {
           const data = await getData(url);
-          const color = await ColorThief.getColor(data["url"]);
 
           const embed = new EmbedBuilder()
             .setTitle(`${data["title"]} (${data["date"]})`)
-            .setDescription(`${data["explanation"]}`)
-            .setImage(data["url"])
-            .setColor(color);
+            .setDescription(`${data["explanation"]}`);
+
+          if (data["media_type"] == "video")
+            embed
+              .setURL(data["url"])
+              .setImage(data["thumbnail_url"])
+              .setColor("DarkAqua");
+          else
+            embed
+              .setImage(data["url"])
+              .setColor(await ColorThief.getColor(data["url"]));
 
           await interaction.reply({ embeds: [embed.toJSON()] });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      fetchData(url);
+    }
+
+    if (subcommand === "range") {
+      let count = interaction.options.get("count") || 1;
+
+      if (count !== null) {
+        if (count.value <= 9) {
+          url += `&count=${count.value}`;
+        } else url += `&count=9`;
+      } else url += `&count=1`;
+
+      async function fetchData(url) {
+        try {
+          const data = await getData(url);
+          let embed = new EmbedBuilder().setTitle(
+            `${count.value} randomnly chosen APOD(s)`
+          );
+
+          data.forEach(function (value) {
+            embed.addFields({
+              name: value["title"],
+              value: value["date"],
+              inline: true,
+            });
+          });
+
+          await interaction.reply({ embeds: [embed] });
         } catch (error) {
           console.log(error);
         }
@@ -91,7 +128,7 @@ module.exports = {
 
   data: new SlashCommandBuilder()
     .setName("apod")
-    .setDescription("Astronomy picture of the day")
+    .setDescription("Astronomy picture of the day.")
     .addSubcommand((subcommand) =>
       subcommand
         .setName("date")
@@ -101,6 +138,27 @@ module.exports = {
             .setName("date")
             .setDescription(
               "Get the picture of the specified date (YYYY-MM-DD) or if nothing specified the current date."
+            )
+            .setRequired(false)
+        )
+        .addBooleanOption((option) =>
+          option
+            .setName("description")
+            .setDescription("Should there be a description (default true).")
+            .setRequired(false)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("range")
+        .setDescription(
+          "Get the date and title of a specified amount of randomnly chosen APODs"
+        )
+        .addIntegerOption((option) =>
+          option
+            .setName("count")
+            .setDescription(
+              "Amount for randomnly chosen images (max. 9) or if nothing specified 1."
             )
             .setRequired(false)
         )
